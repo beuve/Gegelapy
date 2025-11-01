0456cadfb212fc873d227a5f9ba51a944cc0314f
“beuve” <nicolas@beuve.net>
2025-11-01 13:36:07 +0100

    Put all instructions related function in the instruction.h file

--- src/gegelapy.cpp
+++ src/gegelapy.cpp
@@ -13,21 +13,7 @@ namespace py = pybind11;
 PYBIND11_MODULE(gegelapy, m) {
   m.doc() = "Python bindings for Gegelati with Gym integration";
 
-  py::class_<Instructions::Instruction>(m, "Instruction");
-  py::class_<Instructions::LambdaInstruction<double>, Instructions::Instruction>(m, "LambdaInstruction1");
-  py::class_<Instructions::LambdaInstruction<double, double>, Instructions::Instruction>(m, "LambdaInstruction2");
-
-  m.attr("add") = py::cast(i_add);
-  m.attr("sub") = py::cast(i_sub);
-  m.attr("div") = py::cast(i_div);
-  m.attr("mul") = py::cast(i_mul);
-  m.attr("exp") = py::cast(i_exp);
-  m.attr("ln") = py::cast(i_ln);
-  m.attr("max") = py::cast(i_max);
-  m.attr("sin") = py::cast(i_sin);
-  m.attr("cos") = py::cast(i_cos);
-  m.attr("tan") = py::cast(i_tan);
-  m.attr("pi") = py::cast(i_pi);
+  add_instructions(m);
 
   py::class_<GymTrainer>(m, "TPG")
     .def(py::init<GymEnvironment&, const std::vector<Instructions::Instruction*>&>(),
--- src/instructions.h
+++ src/instructions.h
@@ -4,30 +4,59 @@
 #include <cmath>
 #include <algorithm>
 
-// Lambdas
-auto f_add = [](double a, double b) -> double { return a + b; };
-auto f_sub = [](double a, double b) -> double { return a - b; };
-auto f_mul = [](double a, double b) -> double { return a * b; };
-auto f_div = [](double a, double b) -> double { return a / b; };
-auto f_max = [](double a, double b) -> double { return std::max(a, b); };
-auto f_ln = [](double a) -> double { return std::log(a); };
-auto f_exp = [](double a) -> double { return std::exp(a); };
-auto f_cos = [](double a) -> double { return std::cos(a); };
-auto f_sin = [](double a) -> double { return std::sin(a); };
-auto f_tan = [](double a) -> double { return std::tan(a); };
-auto f_pi = [](double) -> double { return M_PI; };
-auto f_multByConst = [](double a, Data::Constant c) -> double { return a * (double)c / 10.0; };
+namespace py = pybind11;
 
-// LambdaInstruction instances
-auto i_add = Instructions::LambdaInstruction<double,double>(f_add, "$0 = $1 + $2;");
-auto i_sub = Instructions::LambdaInstruction<double,double>(f_sub, "$0 = $1 - $2;");
-auto i_mul = Instructions::LambdaInstruction<double,double>(f_mul, "$0 = $1 * $2;");
-auto i_div = Instructions::LambdaInstruction<double,double>(f_div, "$0 = $1 / $2;");
-auto i_max = Instructions::LambdaInstruction<double, double>(f_max, "$0 = (($1) < ($2)) ? ($2) : ($1);");
-auto i_exp = Instructions::LambdaInstruction<double>(f_exp, "$0 = exp($1);");
-auto i_ln = Instructions::LambdaInstruction<double>(f_ln, "$0 = log($1);");
-auto i_cos = Instructions::LambdaInstruction<double>(f_cos, "$0 = cos($1);");
-auto i_sin = Instructions::LambdaInstruction<double>(f_sin, "$0 = sin($1);");
-auto i_tan = Instructions::LambdaInstruction<double>(f_tan, "$0 = tan($1);");
-auto i_pi = Instructions::LambdaInstruction<double>(f_pi, "$0 = M_PI;");
-auto i_multByConst = Instructions::LambdaInstruction<double, Data::Constant>(f_multByConst, "$0 = $1 * ((double)($2) / 10.0);");
+void add_instructions(py::module_ m) {
+
+  py::class_<Instructions::Instruction>(m, "Instruction");
+  py::class_<Instructions::LambdaInstruction<double>, Instructions::Instruction>(m, "LambdaInstruction1");
+  py::class_<Instructions::LambdaInstruction<double, double>, Instructions::Instruction>(m, "LambdaInstruction2");
+
+  auto f_add = [](double a, double b) -> double { return a + b; };
+  auto i_add = Instructions::LambdaInstruction<double,double>(f_add, "$0 = $1 + $2;");
+  m.attr("add") = py::cast(i_add);
+
+  auto f_sub = [](double a, double b) -> double { return a - b; };
+  auto i_sub = Instructions::LambdaInstruction<double,double>(f_sub, "$0 = $1 - $2;");
+  m.attr("sub") = py::cast(i_sub);
+
+  auto f_mul = [](double a, double b) -> double { return a * b; };
+  auto i_mul = Instructions::LambdaInstruction<double,double>(f_mul, "$0 = $1 * $2;");
+  m.attr("mul") = py::cast(i_mul);
+
+  auto f_div = [](double a, double b) -> double { return a / b; };
+  auto i_div = Instructions::LambdaInstruction<double,double>(f_div, "$0 = $1 / $2;");
+  m.attr("div") = py::cast(i_div);
+
+  auto f_max = [](double a, double b) -> double { return std::max(a, b); };
+  auto i_max = Instructions::LambdaInstruction<double, double>(f_max, "$0 = (($1) < ($2)) ? ($2) : ($1);");
+  m.attr("max") = py::cast(i_max);
+
+  auto f_min = [](double a, double b) -> double { return std::min(a, b); };
+  auto i_min = Instructions::LambdaInstruction<double, double>(f_min, "$0 = (($1) < ($2)) ? ($1) : ($2);");
+  m.attr("min") = py::cast(i_min);
+
+  auto f_ln = [](double a) -> double { return std::log(a); };
+  auto i_ln = Instructions::LambdaInstruction<double>(f_ln, "$0 = log($1);");
+  m.attr("ln") = py::cast(i_ln);
+
+  auto f_exp = [](double a) -> double { return std::exp(a); };
+  auto i_exp = Instructions::LambdaInstruction<double>(f_exp, "$0 = exp($1);");
+  m.attr("exp") = py::cast(i_exp);
+
+  auto f_cos = [](double a) -> double { return std::cos(a); };
+  auto i_cos = Instructions::LambdaInstruction<double>(f_cos, "$0 = cos($1);");
+  m.attr("cos") = py::cast(i_cos);
+
+  auto f_sin = [](double a) -> double { return std::sin(a); };
+  auto i_sin = Instructions::LambdaInstruction<double>(f_sin, "$0 = sin($1);");
+  m.attr("sin") = py::cast(i_sin);
+
+  auto f_tan = [](double a) -> double { return std::tan(a); };
+  auto i_tan = Instructions::LambdaInstruction<double>(f_tan, "$0 = tan($1);");
+  m.attr("tan") = py::cast(i_tan);
+
+  auto f_pi = [](double) -> double { return M_PI; };
+  auto i_pi = Instructions::LambdaInstruction<double>(f_pi, "$0 = M_PI;");
+  m.attr("pi") = py::cast(i_pi);
+}
